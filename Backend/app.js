7103cfea1660c3180d217f27d674ee6c1a088658
Ankit Jangir <knjangir24@gmail.com>
2024-12-03 00:47:53 +0530

    captain part done

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+
 connectToDb();
 
 app.use(cors());
@@ -13,11 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 
 
